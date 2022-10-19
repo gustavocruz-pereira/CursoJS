f7ef6a0d4268ddda08675f3f903c08db2a0bcdc0
Gustavo Cruz <guz.cruz.07@gmail.com>
2022-10-19 15:41:26 -0300

    atualizdo api_rest

--- SECAO 13/src/controllers/HomeController.js
+++ SECAO 13/src/controllers/HomeController.js
@@ -1,14 +1,7 @@
-import Aluno from '../models/Aluno'
-
 class HomeController {
-    async index(req, res) {
-        const novoAluno = await Aluno.create({
-
-        });
-        res.json({
-            "tudoCerto": true,
-        });
-    }
+  async index(req, res) {
+    res.json('Index');
+  }
 }
 
 export default new HomeController();
